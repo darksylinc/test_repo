6abb62aad227db75bc9118032e7ff6fc80a8cbab
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-01-10 19:57:24 -0300

    Fixes

--- .github/workflows/test_script.sh
+++ .github/workflows/test_script.sh
@@ -1,4 +1,5 @@
 echo "--- Installing System Dependencies ---"
+sudo apt-get update
 sudo apt-get install -y ninja-build libxrandr-dev libxaw7-dev libgl1-mesa-dev libglu1-mesa-dev libsdl2-dev
 
-ll /usr/bin/
+ls /usr/bin/
