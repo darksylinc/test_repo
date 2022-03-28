ee5c4405090f5eb8bd7657be37c581e9cec7f7b4
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-03-27 23:22:01 -0300

    ASd

--- .github/workflows/test_script2.sh
+++ .github/workflows/test_script2.sh
@@ -1,3 +1,9 @@
 echo "Hello"
 echo "$1"
-echo "$GITHUB_BASE_REF"
+if [ -z $1 || $1 == "" ]; then
+	echo "PR is empty";
+else
+	# Pull Requests can't use git branch, so Github provides it for us
+	echo "PR has something";
+	echo "$1";
+fi
