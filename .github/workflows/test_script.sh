0c905472d3e947b51ee84748ae581cfa0de8dad2
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-03-27 23:17:43 -0300

    T

--- .github/workflows/test_script.sh
+++ .github/workflows/test_script.sh
@@ -1,5 +1,5 @@
 echo "--- Installing System Dependencies ---"
-sudo apt-get update
-sudo apt-get install -y ninja-build libxrandr-dev libxaw7-dev libgl1-mesa-dev libglu1-mesa-dev libsdl2-dev
+#sudo apt-get update
+#sudo apt-get install -y ninja-build libxrandr-dev libxaw7-dev libgl1-mesa-dev libglu1-mesa-dev libsdl2-dev
 
-ls /usr/bin/
+#ls /usr/bin/
---
+++ .github/workflows/test_script2.sh
@@ -0,0 +1,3 @@
+echo "Hello"
+echo "$1"
+echo "$GITHUB_BASE_REF"
