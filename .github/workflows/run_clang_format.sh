727a72eb5896acc73641f19588ffbe9c5065aee7
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-07-17 01:53:23 -0300

    dsa

--- .github/workflows/run_clang_format.sh
+++ .github/workflows/run_clang_format.sh
@@ -4,6 +4,11 @@ echo "--- Installing System Dependencies ---"
 sudo apt-get update
 sudo apt-get install -y clang-format-13
 
+echo "--- Fetching commits to base PR ---"
+prCommits=`gh pr view $prId --json commits | jq '.commits | length'`
+fetchDepthToPrBase=`expr $prCommits + 2`
+git fetch --no-tags --prune --progress --no-recurse-submodules --deepen=$fetchDepthToPrBase
+
 echo "--- Running Clang Format Script ---" 
 python3 run_clang_format.py $2 || exit $?
 
