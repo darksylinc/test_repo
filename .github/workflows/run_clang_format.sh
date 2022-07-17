9994bf15a260a1917a855075439d8c09b77aa59c
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-07-17 01:58:32 -0300

    Fix hash

--- .github/workflows/run_clang_format.sh
+++ .github/workflows/run_clang_format.sh
@@ -7,6 +7,7 @@ sudo apt-get install -y clang-format-13
 echo "--- Fetching commits to base PR ---"
 prCommits=`gh pr view $prId --json commits | jq '.commits | length'`
 fetchDepthToPrBase=`expr $prCommits + 2`
+echo "fetchDepthToPrBase: $fetchDepthToPrBase"
 git fetch --no-tags --prune --progress --no-recurse-submodules --deepen=$fetchDepthToPrBase
 
 echo "--- Running Clang Format Script ---" 
